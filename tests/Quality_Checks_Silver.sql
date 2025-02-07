88d6db9c10f471c88b3ee39fccd9eb370eabca83
dothang08 <125253256+dothang08@users.noreply.github.com>
2025-02-07 08:16:56 +0700

    Update Quality_Checks_Silver.sql

--- tests/Quality_Checks_Silver.sql
+++ tests/Quality_Checks_Silver.sql
@@ -22,23 +22,20 @@ Usage Notes:
 -- ====================================================================
 -- Check for NULLs or Duplicates in Primary Key
 -- Expectation: No Results
-SELECT 
-    cst_id,
-    COUNT(*) 
+SELECT cst_id
+    , COUNT(*) 
 FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
 -- Check for Unwanted Spaces
 -- Expectation: No Results
-SELECT 
-    cst_key 
+SELECT cst_key 
 FROM silver.crm_cust_info
 WHERE cst_key != TRIM(cst_key);
 
 -- Data Standardization & Consistency
-SELECT DISTINCT 
-    cst_marital_status 
+SELECT DISTINCT cst_marital_status 
 FROM silver.crm_cust_info;
 
 -- ====================================================================
@@ -46,36 +43,31 @@ FROM silver.crm_cust_info;
 -- ====================================================================
 -- Check for NULLs or Duplicates in Primary Key
 -- Expectation: No Results
-SELECT 
-    prd_id,
-    COUNT(*) 
+SELECT prd_id
+    , COUNT(*) 
 FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
 
 -- Check for Unwanted Spaces
 -- Expectation: No Results
-SELECT 
-    prd_nm 
+SELECT prd_nm 
 FROM silver.crm_prd_info
 WHERE prd_nm != TRIM(prd_nm);
 
 -- Check for NULLs or Negative Values in Cost
 -- Expectation: No Results
-SELECT 
-    prd_cost 
+SELECT nprd_cost 
 FROM silver.crm_prd_info
 WHERE prd_cost < 0 OR prd_cost IS NULL;
 
 -- Data Standardization & Consistency
-SELECT DISTINCT 
-    prd_line 
+SELECT DISTINCT prd_line 
 FROM silver.crm_prd_info;
 
 -- Check for Invalid Date Orders (Start Date > End Date)
 -- Expectation: No Results
-SELECT 
-    * 
+SELECT * 
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
 
@@ -84,8 +76,7 @@ WHERE prd_end_dt < prd_start_dt;
 -- ====================================================================
 -- Check for Invalid Dates
 -- Expectation: No Invalid Dates
-SELECT 
-    NULLIF(sls_due_dt, 0) AS sls_due_dt 
+SELECT NULLIF(sls_due_dt, 0) AS sls_due_dt 
 FROM bronze.crm_sales_details
 WHERE sls_due_dt <= 0 
     OR LEN(sls_due_dt) != 8 
@@ -94,18 +85,16 @@ WHERE sls_due_dt <= 0
 
 -- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
 -- Expectation: No Results
-SELECT 
-    * 
+SELECT * 
 FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt 
    OR sls_order_dt > sls_due_dt;
 
 -- Check Data Consistency: Sales = Quantity * Price
 -- Expectation: No Results
-SELECT DISTINCT 
-    sls_sales,
-    sls_quantity,
-    sls_price 
+SELECT DISTINCT sls_sales
+    , sls_quantity
+    , sls_price 
 FROM silver.crm_sales_details
 WHERE sls_sales != sls_quantity * sls_price
    OR sls_sales IS NULL 
@@ -121,23 +110,20 @@ ORDER BY sls_sales, sls_quantity, sls_price;
 -- ====================================================================
 -- Identify Out-of-Range Dates
 -- Expectation: Birthdates between 1924-01-01 and Today
-SELECT DISTINCT 
-    bdate 
+SELECT DISTINCT bdate 
 FROM silver.erp_cust_az12
 WHERE bdate < '1924-01-01' 
    OR bdate > GETDATE();
 
 -- Data Standardization & Consistency
-SELECT DISTINCT 
-    gen 
+SELECT DISTINCT gen 
 FROM silver.erp_cust_az12;
 
 -- ====================================================================
 -- Checking 'silver.erp_loc_a101'
 -- ====================================================================
 -- Data Standardization & Consistency
-SELECT DISTINCT 
-    cntry 
+SELECT DISTINCT cntry 
 FROM silver.erp_loc_a101
 ORDER BY cntry;
 
@@ -146,14 +132,12 @@ ORDER BY cntry;
 -- ====================================================================
 -- Check for Unwanted Spaces
 -- Expectation: No Results
-SELECT 
-    * 
+SELECT * 
 FROM silver.erp_px_cat_g1v2
 WHERE cat != TRIM(cat) 
    OR subcat != TRIM(subcat) 
    OR maintenance != TRIM(maintenance);
 
 -- Data Standardization & Consistency
-SELECT DISTINCT 
-    maintenance 
+SELECT DISTINCT maintenance 
 FROM silver.erp_px_cat_g1v2;
